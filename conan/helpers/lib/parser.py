89af21c8bd94cf8a0722ac27602fbe5fda7ed154
Andrew Marshall <andrew.marshall3@sky.uk>
2026-09-01 16:47:50 +0100

    add python helper for getting the conan dependency graph

--- conan/helpers/lib/parser.py
+++ conan/helpers/lib/parser.py
@@ -2,12 +2,25 @@ from conan.api.conan_api import ConanAPI
 
 from pathlib import Path
 
-def parse_lockfile(lockfile: Path):
 
+def dependencies(conanfile: Path):
     api = ConanAPI()
-    lock = api.lockfile.get_lockfile(lockfile)
-    for dep in lock:
-        print(dep)
 
+    host_profile = api.profiles.get_profile([api.profiles.get_default_host()])
+    build_profile = api.profiles.get_profile([api.profiles.get_default_build()])
+    conancenter = api.remotes.get("conancenter")
 
-
+    args = OrderedDict({
+        "path": conanfile_path.as_posix(),
+        "name": None,
+        "version": None,
+        "user": None,
+        "channel": None,
+        "profile_host": host_profile,
+        "profile_build": build_profile,
+        "lockfile": None,
+        "remotes": [conancenter],
+        "update": None,
+    })
+    graph = api.graph.load_graph_consumer(*args.values())
+    deps = [ { "name": node.name, "version": str(node.ref.version) } for node in graph.nodes[1:] ]
